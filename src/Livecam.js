15925205ffdf1e307c04f9386704bb1a687d4c5e
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-01-03 10:58:20 +0900

    layered image in clock order

--- src/Livecam.js
+++ src/Livecam.js
@@ -1,35 +1,57 @@
 import React, { Component } from "react";
-import Client from "./helpers/api";
+import earth from "./assets/earth.png";
+import { livecamData, clock } from "./helpers/livecamdata";
 
 class Livecam extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      livecam: []
+      livecam: livecamData
     };
   }
 
   componentDidMount() {
-    Client.liveCamSearch(data => {
-      this.setState({ livecam: data });
+    this.addCanvas();
+  }
+
+  addCanvas() {
+    const layer1 = document.getElementById("layer1");
+    const ctx1 = layer1.getContext("2d");
+
+    /*const img = new Image();
+    img.src =
+      "https://www.fourmilab.ch/cgi-bin/Earth?img=NASAmMM-l.evif&imgsize=550&dynimg=y&opt=-l&lat=90&ns=North&lon=0&ew=East&alt=35785&tle=&date=0&utc=&jd=";
+    img.onload = function(e) {
+      ctx1.drawImage(img, 400, 80);
+    };
+    */
+
+    const layer2 = document.getElementById("layer2");
+    const ctx2 = layer2.getContext("2d");
+
+    this.state.livecam.forEach(data => {
+      const cam_img = new Image();
+      cam_img.src = `https://images.webcams.travel/thumbnail/${data.id}.jpg`;
+      cam_img.onload = function(e) {
+        ctx2.drawImage(cam_img, data.pos[0], data.pos[1], 80, 45);
+      };
     });
   }
 
   render() {
-    const webcamImages = this.state.livecam.map(id => (
-      <img
-        src={`https://images.webcams.travel/thumbnail/${id}.jpg`}
-        key={id}
-        alt={"livecam"}
-      />
-    ));
     return (
       <div>
-        {webcamImages}
-        <img
-          src={
-            "https://www.fourmilab.ch/cgi-bin/Earth?img=NASAmMM-l.evif&imgsize=600&dynimg=y&opt=-l&lat=90&ns=North&lon=0&ew=East&alt=35785&tle=&date=0&utc=&jd="
-          }
+        <canvas
+          id="layer1"
+          width="1200"
+          height="800"
+          style={{ zIndex: 0, left: 0, top: 0, position: "absolute" }}
+        />
+        <canvas
+          id="layer2"
+          width="1200"
+          height="800"
+          style={{ zIndex: 100, left: 0, top: 0, position: "absolute" }}
         />
       </div>
     );
---
+++ src/helpers/livecamdata.js
@@ -0,0 +1,77 @@
+const clock = [
+  [630, 25],
+  [830, 80],
+  [930, 180],
+  [960, 320],
+  [935, 460],
+  [850, 580],
+  [630, 650],
+  [410, 560],
+  [300, 320],
+  [300, 450],
+  [320, 180],
+  [400, 80]
+];
+
+export const livecamData = [
+  {
+    id: 1425411457,
+    url: "https://www.lookr.com/lookout/1425411457",
+    pos: clock[0]
+  },
+  {
+    id: 1326622068,
+    url: "https://www.lookr.com/lookout/1326622068",
+    pos: clock[1]
+  },
+  {
+    id: 1179240123,
+    url: "https://lookr.com/lookout/1179240123",
+    pos: clock[2]
+  },
+  {
+    id: 1368074325,
+    url: "https://lookr.com/lookout/1368074325",
+    pos: clock[3]
+  },
+  {
+    id: 1414620027,
+    url: "https://www.lookr.com/lookout/1414620027",
+    pos: clock[4]
+  },
+  {
+    id: 1169377033,
+    url: "https://www.lookr.com/lookout/1169377033",
+    pos: clock[5]
+  },
+  {
+    id: 1169741426,
+    url: "https://www.lookr.com/lookout/1169741426",
+    pos: clock[6]
+  },
+  {
+    id: 1494492249,
+    url: "https://www.lookr.com/lookout/1494492249",
+    pos: clock[7]
+  },
+  {
+    id: 1225720055,
+    url: "https://www.lookr.com/lookout/1225720055",
+    pos: clock[8]
+  },
+  {
+    id: 1472821743,
+    url: "https://www.lookr.com/lookout/1472821743",
+    pos: clock[9]
+  },
+  {
+    id: 1315507562,
+    url: "https://www.lookr.com/lookout/1315507562",
+    pos: clock[10]
+  },
+  {
+    id: 1359483331,
+    url: "https://www.lookr.com/lookout/1359483331",
+    pos: clock[11]
+  }
+];
